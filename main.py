4cc1f9a9980ed5fff66b322341159986a977b607
luh-dev <lucasguimaraes1905@gmail.com>
2021-01-18 23:04:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# funcionário 1
+
+funcionario_1 = 25
+horas_trabalhadas = 100
+valor_recebido_hr = 5.50
+salario = horas_trabalhadas * valor_recebido_hr
+print('Funcionário 1...')
+print('NUMERO =',funcionario_1)
+print('SALARIO= U$',salario)
+
+print("\n")
+
+# funcionário 2
+
+funcionario_2 = 1
+horas_trabalhadas = 200
+valor_recebido_hr = 20.50
+salario = horas_trabalhadas * valor_recebido_hr
+print('Funcionário 2...')
+print('NUMERO =',funcionario_2)
+print('SALARIO= U$',salario)
+
+print("\n")
+
+# funcionário 3
+
+funcionario_3 = 6
+horas_trabalhadas = 145
+valor_recebido_hr = 15.55
+salario = horas_trabalhadas * valor_recebido_hr
+print('Funcionário 3...')
+print('NUMERO =',funcionario_3)
+print('SALARIO= U$', salario)
